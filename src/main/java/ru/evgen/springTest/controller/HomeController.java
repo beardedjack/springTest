f6bc682ab15246286113aeaf9e06faa10b889214
kondrashov <evgen.kondrashoff@gmail.com>
2016-10-14 16:55:05 +0400

    add jdbc

--- src/main/java/ru/evgen/springTest/controller/HomeController.java
+++ src/main/java/ru/evgen/springTest/controller/HomeController.java
@@ -4,8 +4,7 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,7 +19,7 @@ import ru.evgen.springTest.model.User;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -40,7 +39,7 @@ public class HomeController {
 	
 	 @RequestMapping(value = "/", method = RequestMethod.GET)
 	    public ModelAndView main(Locale locale) {
-		 	logger.info("Welcome home! The client locale is {}.", locale);
+		 	
 			Date date = new Date();
 			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 			String formattedDate = dateFormat.format(date);
---
+++ src/main/java/ru/evgen/springTest/dao/ContactDAO.java
@@ -0,0 +1,21 @@
+package ru.evgen.springTest.dao;
+
+import java.util.List;
+
+import ru.evgen.springTest.model.Contact;
+
+/**
+ * Defines DAO operations for the contact model.
+ * @author www.codejava.net
+ *
+ */
+public interface ContactDAO {
+	
+	public void saveOrUpdate(Contact contact);
+	
+	public void delete(int contactId);
+	
+	public Contact get(int contactId);
+	
+	public List<Contact> list();
+}
